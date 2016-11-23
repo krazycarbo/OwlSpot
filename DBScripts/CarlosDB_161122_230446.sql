ec49ad067c2be28950e7ae8bc38f626d6d883221
kmaraj2013 <kmaraj2013@fau.edu>
2016-11-22 23:31:22 -0500

    Added CarlosDB

--- DBScripts/CarlosDB_161122_230446.sql
+++ DBScripts/CarlosDB_161122_230446.sql
@@ -1,14 +1,14 @@
--- Problems [ent2]
-create table [dbo].[PROBLEMS] (
+-- Activities [ent3]
+create table [dbo].[ACTIVITIES] (
    [OID]  int  not null,
+   [FIRST_NAME]  varchar(255),
    [IMAGE]  varchar(255),
    [DESCRIPTION]  varchar(255),
-   [INCIDENT_TYPE]  varchar(255),
+   [ACTIVITY_TYPE]  varchar(255),
    [DATE]  date,
    [Z_NUMBER]  varchar(255),
    [EMAIL]  varchar(255),
    [LAST_NAME]  varchar(255),
-   [FIRST_NAME]  varchar(255),
   primary key ([OID])
 );
 
